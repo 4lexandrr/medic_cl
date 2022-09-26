00845e2c87d986b1b244d68357d5887ad0de4594
4lexandr <4lexandrrr@mail.ru>
2022-09-26 20:38:29 +0300

    first_commit, version 1. init

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from base.models import Person
+from base.models import Client
+from base.models import Venue
+from base.models import Events
+from base.models import Employees
+from base.models import Post
+from base.models import Type_of_diagnoses
+from base.models import Visits
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Client)
+admin.site.register(Venue)
+admin.site.register(Events)
+admin.site.register(Employees)
+admin.site.register(Post)
+admin.site.register(Type_of_diagnoses)
+admin.site.register(Visits)
--- base/views.py
+++ base/views.py
@@ -1,23 +1,136 @@
-from django.shortcuts import render
-
+import imp
+from unicodedata import name
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from .forms import LoginForm
+from .models import Client, Venue
+from .models import Events
+from .forms import EventForm
+from .forms import VenueForm
 # Create your views here.
 
 from django.http import HttpResponse
 
-pages = [
-    {'id': 1, 'name': 'Личный кабинет'},
-    {'id': 2, 'name': 'Запись к врачу'},
-]
+
+def all_venue(request):
+    venue_list = Venue.objects.all()
+    context = {'venue_list': venue_list}
+    return render(request, 'base/organization.html', context)
+
+
+def add_venue(request):
+    submitted = False
+    if request.method == 'POST':
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'base/add_organization.html', {'form': form, 'submitted': submitted})
+
+
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None, instance=venue)
+    context = {'venue': venue, 'form': form}
+
+    if form.is_valid():
+        form.save()
+        return redirect('all-venue')
+
+    return render(request, 'base/update_organization.html', context)
+
+
+def delete_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    context = {'obj': venue}
+    if request.method == 'POST':
+        venue.delete()
+        return redirect('all-venue')
+
+    return render(request, 'base/delete.html', context)
+
+
+def delete_events(request, event_id):
+    event = Events.objects.get(pk=event_id)
+    context = {'obj': event}
+    if request.method == 'POST':
+        event.delete()
+        return redirect('list-events')
+
+    return render(request, 'base/delete.html', context)
+
+def update_events(request, event_id):
+    event = Events.objects.get(pk=event_id)
+    form = EventForm(request.POST or None, instance=event)
+    context = {'event': event, 'form': form}
+
+    if form.is_valid():
+        form.save()
+        return redirect('list-events')
+
+    return render(request, 'base/update_events.html', context)
+
+
+
+def add_events(request):
+    submitted = False
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_events?submitted=True')
+
+    else:
+        form = EventForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'base/add_events.html', {'form': form, 'submitted': submitted})
+
+    
+def personal(request):
+    profile = Client.objects.all()
+    context = {'profile': profile}
+    return render(request, 'base/personal_cab.html', context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('home')
+                else:
+                    messages.error(request, 'Неверный логин и/или пароль')
+            else:
+                messages.error(request, 'Неверный логин и/или пароль')
+    else:
+        form = LoginForm()
+        
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
-    context = {'pages': pages}
-    return render(request, 'base/home.html', context)
+    return render(request, 'base/home.html')
 
-def room(request, pk):
-    page = None
-    for i in pages:
-        if i['id'] == int(pk):
-            page = i
-    context = {'page': page}
-    return render(request, 'base/room.html', context)
+
+def all_events(request):
+    event_list = Events.objects.all()
+    context = {'event_list': event_list}
+    return render(request, 'base/events_list.html', context)
+
+
+
